ab13bce4ec57afbddeedd54bd3290208e2a202c3
Jennifer <jennifer.elaine.mairead@gmail.com>
2018-01-16 15:56:18 +0000

    end of webapp intro

--- app.rb
+++ app.rb
@@ -20,11 +20,15 @@ get '/random-cat' do
   erb(:index)
 end
 
-get '/named-cat' do
+post '/named-cat' do
   p params
   @name = params[:name]
-  @colour = params[:colour]
+  # @colour = params[:colour]
   erb(:index)
+end
+
+get '/cat-form' do
+  erb(:cat_form)
 end
 
 
